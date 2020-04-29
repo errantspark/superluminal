b493651894af8fba41706207649fc5894d9f282a
errantspark <errantspark@gmail.com>
2020-04-29 03:08:20 -0700

    now there's actual templating and shit

--- render.js
+++ render.js
@@ -1,7 +1,18 @@
+import fs from 'fs'
 import marked from 'marked'
+
+let applyTemplate = template => object => {
+  let keys = Object.keys(object)
+  return new Function('input' , `let {${keys.join(',')}} = input
+return \`${template}\``)(object)
+}
+
 
 // Set options
 // `highlight` example uses `highlight.js`
+let template = fs.readFileSync('./default.html').toString()
+let stylesheet = fs.readFileSync('./default.css').toString()
+let script = fs.readFileSync('./default.js').toString()
 marked.setOptions({
   renderer: new marked.Renderer(),
   /*
@@ -20,7 +31,7 @@ marked.setOptions({
   xhtml: false
 });
 
-let preProcessor = (template, object = {}) => {
+let preProcess = (template, object = {}) => {
   template += ""
   let keys = Object.keys(object)
   return new Function('input' , `let {${keys.join(',')}} = input
@@ -28,6 +39,14 @@ return \`${template.replace(/`/gim,"\\\`")}\``)(object)
 }
 
 
-const render = string => marked(preProcessor(string))
+const render = (jsMarkdownString, filename) => {
+  let body = marked(preProcess(jsMarkdownString))
+  return applyTemplate(template)({
+    title: filename,
+    stylesheet,
+    script,
+    body
+  })
+}
 
 export default render
